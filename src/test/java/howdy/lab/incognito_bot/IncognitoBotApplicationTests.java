dd64e3879af83a6155c2b62a14f1537652bb651c
Suhrob <suhrob_kalandarov@outlook.com>
2026-02-24 00:04:36 +0500

    feat: initial commit

---
+++ src/test/java/howdy/lab/incognito_bot/IncognitoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package howdy.lab.incognito_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncognitoBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
